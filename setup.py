8c2192b61492aa46f1d8518c46f4fcf2200e553c
cyrus <cyruscyliu@gmail.com>
2019-12-13 15:52:13 +0800

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pymkparser',
+    version='1.0.0',
+    packages=['pymkparser'],
+    url='https://github.com/cyruscyliu/pymake',
+    license='MIT',
+    author='cyruscyliu',
+    author_email='cyruscyliu@gmail.com',
+    description='a makefile parser from moliza/pymake'
+)
--- tests/include-glob.mk
+++
@@ -1,9 +0,0 @@
-include $(TESTPATH)/glob*.inc
-
-ifeq ($A_$B,ok_ok)
-all:
-	@echo TEST-PASS
-else
-all:
-	@echo TEST-FAIL
-endif
